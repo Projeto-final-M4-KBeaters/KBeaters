59b7ed2253c07da12549f6ff1ef8c2a090a82055
IgorD934 <igord934@gmail.com>
2023-01-12 10:31:24 -0300

    Create listAllPerformersRoutes

--- src/controllers/users/registerUser.controller.ts
+++ src/controllers/users/registerUser.controller.ts
@@ -1,13 +1,11 @@
-import { Request, Response } from "express"
-import { IUserRequest } from "../../interfaces/users"
-import { registerUserService } from "../../services/users/registerUsers.service"
-
-
+import { Request, Response } from "express";
+import { IUserRequest } from "../../interfaces/users";
+import registerUserService from "../../services/users/registerUsers.service";
 
 const createUserController = async (req: Request, res: Response) => {
-    const userData: IUserRequest = req.body
-    const newUser = await registerUserService(userData)
-    return res.status(201).json(newUser)
-}
+  const userData: IUserRequest = req.body;
+  const newUser = await registerUserService(userData);
+  return res.status(201).json(newUser);
+};
 
-export {createUserController}
+export default createUserController;
--- src/routes/users/registerUsers.routes.ts
+++ src/routes/users/registerUsers.routes.ts
@@ -1,11 +1,16 @@
-import { Router } from "express"
-import { createUserController } from "../../controllers/users/registerUser.controller"
-import { ensureDataIsValidMiddleware } from "../../middlewares/ensureDataIsValid.middleware"
-import { userSerializer } from "../../serializers/users"
+import { Router } from "express";
+import listAllPerformersController from "../../controllers/users/listAllPerformers.controller";
+import createUserController from "../../controllers/users/registerUser.controller";
+import { ensureDataIsValidMiddleware } from "../../middlewares/ensureDataIsValid.middleware";
+import { userSerializer } from "../../serializers/users";
 
+const userRoutes = Router();
 
-const userRoutes = Router()
+userRoutes.post(
+  "",
+  ensureDataIsValidMiddleware(userSerializer),
+  createUserController
+);
+userRoutes.get("/performer", listAllPerformersController);
 
-userRoutes.post('',ensureDataIsValidMiddleware(userSerializer), createUserController)
-
-export {userRoutes}
+export { userRoutes };
