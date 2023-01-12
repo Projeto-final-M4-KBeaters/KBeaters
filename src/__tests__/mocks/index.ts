b06dcc71a43d3ff6d1277e8018990362c3ba0cda
lucas <lucascostaschmidt@gmail.com>
2023-01-12 12:35:16 -0300

    Created registerPOST/ Fixed entitie users, missing name

--- src/__tests__/mocks/index.ts
+++ src/__tests__/mocks/index.ts
@@ -1,4 +1,24 @@
-import { IUserLogin } from "../../interfaces/users"
+import { IUserLogin, IUserRequest } from "../../interfaces/users"
+
+const mockedUserRegister : IUserRequest={
+    name: "JP",
+    email: "jp@mail.com",
+    password: "123456",
+    isPerformer: false,
+}
+
+const mockedAdminRegister : IUserRequest={
+    name: "lucas",
+    email: "schmitao@mail.com",
+    password: "123456",
+}
+
+const mockedPerformerRegister : IUserRequest={
+    name: "lucas",
+    email: "schmitao@mail.com",
+    password: "123456",
+    isPerformer: true
+}
 
 const mockedUserLogin : IUserLogin = {
     email: "jp@mail.com",
@@ -15,4 +35,4 @@ const mockedPerformerLogin : IUserLogin = {
     password: "123456"
 }
 
-export {mockedAdminLogin, mockedPerformerLogin, mockedUserLogin}
+export {mockedAdminLogin, mockedPerformerLogin, mockedUserLogin, mockedUserRegister,mockedAdminRegister, mockedPerformerRegister}
---
+++ src/services/users/registerUsers.service.ts
@@ -0,0 +1,32 @@
+import  AppDataSource  from "../../data-source"
+import { Users } from "../../entities/users.entities"
+import { AppError } from "../../errors"
+import { IUserRequest } from "../../interfaces/users"
+import { userRegisterResponseSerializer } from "../../serializers/users"
+
+
+
+const registerUserService = async (usersData: IUserRequest) => {
+
+    const userRepository = AppDataSource.getRepository(Users)
+
+
+    const userExist = await userRepository.findOneBy({
+        email: usersData.email
+    })
+
+    if (userExist) {
+        throw new AppError('Ja existe', 409)
+    }
+
+    const createdUser = userRepository.create(usersData)
+    await userRepository.save(createdUser)
+    console.log(createdUser)
+    const returnedData = await userRegisterResponseSerializer.validate(createdUser, {
+        stripUnknown: true
+    })
+    
+    return returnedData
+}
+
+export { registerUserService }
