ce177131809fd0e5a91781dfc5d3ecaa47116cea
lucas <lucascostaschmidt@gmail.com>
2023-01-13 12:58:56 -0300

    Adjusting genres files conflict

--- src/routes/genres/genre.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { ensureAuthAdminMiddleware, ensureAuthMiddleware } from "../../middlewares";
-import {listUniqueGenreController} from "../../controllers"
-
-const genreRoutes = Router()
-
-
-genreRoutes.get("/:data",ensureAuthMiddleware, ensureAuthAdminMiddleware, listUniqueGenreController)
