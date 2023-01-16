3a41e1e70c82faee0f722123a250e539ca1c4008
lucas <lucascostaschmidt@gmail.com>
2023-01-16 14:54:28 -0300

    Adjusted genres/tests and duplicated calls in index.ts-controllers and services

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -28,5 +28,4 @@ export {
     listAllMusicsByPerformerController,
     listAllMusicsController,
     listUniqueMusicController,
-    musicsPostController
 };
--- src/services/index.ts
+++ src/services/index.ts
@@ -32,5 +32,4 @@ export{
     listAllMusicsByPerformerService,
     listAllMusicsService,
     listUniqueMusicService,
-    musicsPostService
 };
