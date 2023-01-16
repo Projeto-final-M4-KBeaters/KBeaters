9bc65d89f2331f7f7c06e4d188ffd90cbb190190
IgorD934 <igord934@gmail.com>
2023-01-16 12:21:02 -0300

    Fix: Sequence call of musicRoutes and import middleware

--- src/routes/musics/musics.routes.ts
+++ src/routes/musics/musics.routes.ts
@@ -1,22 +1,15 @@
 import { Router } from "express";
 import { listAllMusicsByGenrerController, deleteMusicController, listAllMusicsByPerformerController, musicsPostController, listUniqueMusicController, listAllMusicsController } from "../../controllers";
-import { ensureAuthIsPerformerMiddleware, ensureAuthMiddleware, ensureDataIsValidMiddleware, ensureExistsGenreMiddleware, ensureMusicNameNotExistsMiddleware, ensureUUIDIsValidMiddleware } from "../../middlewares";
+import { ensureAuthIsAdmOrOwnerMiddleware, ensureAuthIsPerformerMiddleware, ensureAuthMiddleware, ensureDataIsValidMiddleware, ensureExistsGenreMiddleware, ensureMusicNameNotExistsMiddleware, ensureUUIDIsValidMiddleware } from "../../middlewares";
 import { musicsRequestSerializer } from "../../serializers/musics";
 
 const musicsRoutes = Router();
 
-musicsRoutes.get("/genres/:id", ensureUUIDIsValidMiddleware, ensureExistsGenreMiddleware, listAllMusicsByGenrerController)
+musicsRoutes.post("", ensureAuthMiddleware, ensureAuthIsPerformerMiddleware, ensureDataIsValidMiddleware(musicsRequestSerializer), ensureMusicNameNotExistsMiddleware, musicsPostController)
 musicsRoutes.get("", listAllMusicsController)
-musicsRoutes.get(":id", ensureUUIDIsValidMiddleware, listUniqueMusicController)
-musicsRoutes.post(
-    "",
-    ensureAuthMiddleware,
-    ensureAuthIsPerformerMiddleware,
-    ensureDataIsValidMiddleware(musicsRequestSerializer),
-    ensureMusicNameNotExistsMiddleware,
-    musicsPostController
-)
-musicsRoutes.delete("/:id",ensureAuthMiddleware, ensureAuthIsAdmOrOwnerMiddleware, deleteMusicController)
+musicsRoutes.get("/genres/:id", ensureUUIDIsValidMiddleware, ensureExistsGenreMiddleware, listAllMusicsByGenrerController)
 musicsRoutes.get("/performer/:id", ensureUUIDIsValidMiddleware,  listAllMusicsByPerformerController)
+musicsRoutes.get("/:id", ensureUUIDIsValidMiddleware, listUniqueMusicController)
+musicsRoutes.delete("/:id",ensureAuthMiddleware, ensureAuthIsAdmOrOwnerMiddleware, deleteMusicController)
 
 export default musicsRoutes;
