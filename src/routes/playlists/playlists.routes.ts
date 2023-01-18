f09be028c6fd90e395fec308992ede49af6844c5
m4chado <enzo653fm@gmail.com>
2023-01-18 00:13:12 -0300

    create delete playlist route. Ready

--- src/routes/playlists/playlists.routes.ts
+++ src/routes/playlists/playlists.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { listAllPlaylistsController, listUserPlaylistController, listUniquePlaylistController, patchPlaylistsController } from "../../controllers";
+import { listAllPlaylistsController, listUserPlaylistController, listUniquePlaylistController, patchPlaylistsController, deletePlaylistController } from "../../controllers";
 import registerPlaylistsController from "../../controllers/playlists/registerPlaylists.controller";
 import { ensureAdminOrOwnerPlaylistMiddleware, ensureAuthMiddleware, ensureDataIsValidMiddleware, ensurePlaylistExistsMiddleware, ensureUserIdPlaylistIsValidMiddleware, ensureUUIDIsValidMiddleware } from "../../middlewares";
 import { playlistPostSerializer } from "../../serializers/playlists";
@@ -11,7 +11,7 @@ playlistsRoutes.get("", listAllPlaylistsController)
 playlistsRoutes.get("/:id", ensurePlaylistExistsMiddleware, listUniquePlaylistController)
 playlistsRoutes.get("/users/:id", ensureUUIDIsValidMiddleware, ensureUserIdPlaylistIsValidMiddleware, listUserPlaylistController)
 playlistsRoutes.patch("/:id", ensureAuthMiddleware, ensureUUIDIsValidMiddleware, ensureDataIsValidMiddleware(playlistPostSerializer), ensurePlaylistExistsMiddleware, ensureAdminOrOwnerPlaylistMiddleware, patchPlaylistsController)
-
+playlistsRoutes.delete("/:id", ensureAuthMiddleware, ensureAdminOrOwnerPlaylistMiddleware, ensurePlaylistExistsMiddleware,deletePlaylistController)
 
 export default playlistsRoutes;
 
